8267b9653a2dc3aca46c9952f3999ae2d87c4379
AshleyWrightA <AshleyWrightAppDev@outlook.com>
2023-02-06 15:53:17 -0700

    Minor bugfixes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from tkinter import *
-from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 import json
+import tkinter as tk
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -54,8 +54,8 @@ def save():
                 #Saving updated data
                 json.dump(data, data_file, indent=4)
         finally:
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+            website_entry.delete(0, "end")
+            password_entry.delete(0, "end")
 
 
 # ---------------------------- FIND PASSWORD ------------------------------- #
@@ -77,39 +77,40 @@ def find_password():
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-window = Tk()
+window = tk.Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
+#Canvas
+canvas = tk.Canvas(height=200, width=200)
+logo_img = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 #Labels
-website_label = Label(text="Website:")
+website_label = tk.Label(text="Website:")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
+email_label = tk.Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-password_label = Label(text="Password:")
+password_label = tk.Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=21)
+website_entry = tk.Entry(width=21)
 website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = tk.Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
-password_entry = Entry(width=21)
+email_entry.insert(0, "AshW@Email.com")
+password_entry = tk.Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-search_button = Button(text="Search", width=13, command=find_password)
+search_button = tk.Button(text="Search", width=13, command=find_password)
 search_button.grid(row=1, column=2)
-generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button = tk.Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
+add_button = tk.Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
